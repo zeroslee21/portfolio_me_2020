c1561908453496c436d126bf00e6946b8c053e9e
zeroslee21 <zeros.lee@gmail.com>
2020-09-30 13:56:58 +0900

    Updates

---
+++ precache-manifest.6d711e48e6626ff6eab2084325a7a3fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "980213282a86cb0550a3",
+    "url": "/portfolio_me_2020/static/css/main.ef75e75d.chunk.css"
+  },
+  {
+    "revision": "980213282a86cb0550a3",
+    "url": "/portfolio_me_2020/static/js/main.98021328.chunk.js"
+  },
+  {
+    "revision": "d90b75b8e444b8faf512",
+    "url": "/portfolio_me_2020/static/js/runtime~main.d90b75b8.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio_me_2020/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "fc34d7d219c94a6f271b2bec5809b848",
+    "url": "/portfolio_me_2020/index.html"
+  }
+];
