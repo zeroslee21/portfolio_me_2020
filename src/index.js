e155005335a6d43854df46e8b8d1e80a6e8a221f
zeroslee21 <zeros.lee@gmail.com>
2020-09-30 16:14:09 +0900

    1st

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Profile from "views/Profile.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/profile" component={Profile} />
+      <Redirect from="/" to="/profile" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
