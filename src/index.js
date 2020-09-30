73386cff04dfe2da0d71f232f042cff959136bc1
zeroslee21 <zeros.lee@gmail.com>
2020-09-30 19:04:29 +0900

    2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
@@ -9,8 +9,7 @@ import Profile from "views/Profile.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/profile" component={Profile} />
-      <Redirect from="/" to="/profile" />
+      <Route path="/" component={Profile} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
